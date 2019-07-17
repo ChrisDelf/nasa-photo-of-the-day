b6a5d703634c17c0298fa230138d5daa19fef727
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-07-17 18:59:07 -0400

    added more functionality to the youtube player but need to fight out whats wrong with the height

--- src/components/NasaImgCard.js
+++ src/components/NasaImgCard.js
@@ -5,17 +5,20 @@ function NasaCard(props) {
   const {nasaTitle, nasaDate, nasaExp, youtube } = props;
 
 
-  return(
-    <div className ="nasa-card">
-
-    <h2>{nasaTitle}</h2>
-    <h>{nasaDate}</h>
-    <p>{nasaExp}</p>
-    <iframe src = {youtube} frameBorder = '0' allow='autoplay'/>
-
+  return (
+    <div className="nasa-card">
+      <h2 className="App-header">{nasaTitle}</h2>
+      <h>{nasaDate}</h>
+      <p>{nasaExp}</p>
+      <iframe
+        src={youtube}
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+        title="video"
+      />
     </div>
-
-  )
+  );
 
 
 
