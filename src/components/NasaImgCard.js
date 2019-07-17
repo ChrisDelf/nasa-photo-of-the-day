065defb0a98ae6e3697d2a756b7ee81ddad79df9
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-07-17 18:50:26 -0400

    mvp finished

--- src/components/NasaImgCard.js
+++ src/components/NasaImgCard.js
@@ -2,14 +2,17 @@ import React from "react";
 
 
 function NasaCard(props) {
-const {nasaUrl} = props;
+  const {nasaTitle, nasaDate, nasaExp, youtube } = props;
 
 
   return(
     <div className ="nasa-card">
-      <img src={nasaUrl}>
 
-      </img>
+    <h2>{nasaTitle}</h2>
+    <h>{nasaDate}</h>
+    <p>{nasaExp}</p>
+    <iframe src = {youtube} frameBorder = '0' allow='autoplay'/>
+
     </div>
 
   )
--- src/components/NasaImgCon.js
+++ src/components/NasaImgCon.js
@@ -5,24 +5,36 @@ import NasaCard from './NasaImgCard';
 
 
 function NasaImgCon () {
-  const [nasaImg, setNasaImage] = useState([]);
-useEffect(() => {
-  axios.get('https://dog.ceo/api/breed/shiba/images/random')
-    .then(response => {
-      console.log('doggos test', response.data.message);
-      const images = response.data.message;
-      setNasaImage(images);
+  const [nasaData, setNasaData] = useState([]);
 
-  });
- }, [] );
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+      .then(response => {
+        console.log(response.data)
+        const nasaData = response.data
+        console.log("NasaData", nasaData)
+         setNasaData(nasaData)
+      })
+
+
+  },
+  [] )
+
 
 
 
   return (
     <div>
-      Hello
-   <NasaCard key = {nasaImg} nasaUrl = {nasaImg} />
-    </div>
-  );}
 
+         <NasaCard
+            key={nasaData}
+            nasaTitle={nasaData.title}
+            nasaDate={nasaData.date}
+            nasaExp={nasaData.explanation}
+            youtube={nasaData.url}
+          />
+    </div>
+  );
+}
 export default NasaImgCon;
