a6bff65d6160975cc298f6de126f68c4bf1647f0
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-07-18 17:59:02 -0400

    have a general color scheme in place

--- src/StyledComp.js
+++ src/StyledComp.js
@@ -1,17 +1,45 @@
 import styled from "styled-components"
-
+import {colors} from "./StyledComp.js"
 export const Image = styled.img`
 
-  max-width: 350px;
+  max-width: 100%;
   max-height: 600px;
 
 
   `
-export const Iframe = styled.img`
+export const Iframe = styled.iframe`
 
   max-width: 350px;
   max-height: 600px;
 
 
+`
+
+export const Header = styled.header`
+
+  font-size: 1.6rem;
+  background-color: #4a5568;
+  color: #e6fffa;
+  border-radius: 4px;
+  text-Align: center;
+
+
+`
+export const Card = styled.div`
+  width: 18rem;
+  margin:10px;
+  justify-content: center;
+  border: 2px solid  #1a202c;
+  border-radius: 8px;
+  background-color: #e2e8f0;
+  text-Align: left;
+
   `
+
+
+
+
+
+
+
 
--- src/components/NasaImgCard.js
+++ src/components/NasaImgCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Iframe, Image} from "../StyledComp"
+import {Iframe, Image, Header, Card} from "../StyledComp"
 
 
 
@@ -12,18 +12,36 @@ function NasaCard(props) {
 
 
   return (
-    <div className="nasa-card">
-      <h2 className="App-header">{nasaTitle}</h2>
-      <h1>{nasaDate}</h1>
-      <p>{nasaExp}</p>
-   { nasaType === 'video' ? <iframe
-        src={nasaUrl}
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-        title="video"
-       />: <Image src = {nasaUrl}/>}
-    </div>
+    <Card>
+      {nasaType === 'video' ? (
+        <Iframe
+          src={nasaUrl}
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          title="video"
+        />
+      ) : (
+        <Image src={nasaUrl} />
+      )}
+      <div>
+        <Header> {nasaTitle}</Header>
+        <p className="card-text">{nasaExp} </p>
+      </div>
+      <ul>
+        <li> Cras justo odio</li>
+        <li>Dapibus ac facilisis in</li>
+        <li>Vestibulum at eros</li>
+      </ul>
+      <div className="card-body">
+        <a href="#" className="card-link">
+          Card link
+        </a>
+        <a href="#" className="card-link">
+          Another link
+        </a>
+      </div>
+    </Card>
   );
 
 
